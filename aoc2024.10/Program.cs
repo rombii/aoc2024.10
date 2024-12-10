e079e4a28b9fc29eb8144851390cad5b28f58c6a
Kamil Kondziolka <wono83@interia.pl>
2024-12-10 18:23:54 +0100

    Added missing files

---
+++ aoc2024.10/Program.cs
@@ -0,0 +1,42 @@
+﻿var input = File.ReadAllLines(Path.Join(Directory.GetCurrentDirectory(), "input.txt"));
+int part1Answer = 0, part2Answer = 0;
+var reachablePeaks = new HashSet<(int, int)>();
+for (var i = 0; i < input.Length; i++)
+{
+    for (var j = 0; j < input[i].Length; j++)
+    {
+        if (input[i][j] == '0')
+        {
+            part2Answer += HowManyCorrectPaths(i, j);
+            part1Answer += reachablePeaks.Count;
+            reachablePeaks.Clear();
+        }
+    }
+}
+
+Console.WriteLine($"First part: {part1Answer}");
+Console.WriteLine($"Second part: {part2Answer}");
+return;
+
+int HowManyCorrectPaths(int y, int x)
+{
+    var directions = new[] { (-1, 0), (0, 1), (1, 0), (0, -1) };
+    var answer = 0;
+    for (var i = 0; i < directions.Length; i++)
+    {
+        var newY = y + directions[i].Item1;
+        var newX = x + directions[i].Item2;
+        if (newY < 0 || newY >= input.Length || newX < 0 || newX >= input.Length) continue;
+        if (input[newY][newX] != input[y][x] + 1) continue;
+        if (input[newY][newX] == '9')
+        {
+            reachablePeaks.Add((newY, newX));
+            answer++;
+        }
+        else
+        {
+            answer += HowManyCorrectPaths(newY, newX);
+        }
+    }
+    return answer;
+}
